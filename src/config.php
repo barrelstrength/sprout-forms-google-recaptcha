6e0f9ae77adfa54fb917949ebeceb715e35483d6
Ben Parizek <ben.parizek@gmail.com>
2020-04-18 07:34:33 -0400

    Updates example config

--- src/config.php
+++ src/config.php
@@ -1,47 +1,65 @@
 <?php
 
 /**
- * Sprout Forms Google reCAPTCHA config.php
+ * Sprout Forms Google reCAPTCHA config overrides for Sprout Forms
  *
- * This file exists only as a template for the Sprout Forms Google reCAPTCHA settings.
+ * This file exists only as an example for the Sprout Forms Google reCAPTCHA captcha settings.
  * It does nothing on its own.
  *
- * Don't edit this file, instead copy it to 'craft/config' as 'sprout-forms-google-recaptcha.php'
- * and make your changes there to override default settings.
+ * Don't edit this file, instead copy the contents to the 'craft/config/sprout-forms.php'
+ * config file and make your changes there to override default settings.
  *
- * Once copied to 'craft/config', this file will be multi-environment aware as
- * well, so you can have different settings groups for each environment, just as
- * you do for 'general.php'
+ * See the Sprout Forms example config `sprout-forms/src/config.php` for general captcha options.
  */
 
 return [
-    '*' => [
-        // The type of reCAPTCHA to use when validating a form. Default: 'v2_checkbox'
-        //'recaptchaType' => 'v2_checkbox|v2_invisible'
 
-        // The siteKey from your reCAPTCHA settings: https://www.google.com/recaptcha/admin
-        //'siteKey' => '',
+    // ...
 
-        // The secretKey from your reCAPTCHA settings: https://www.google.com/recaptcha/admin
-        //'secretKey' => '',
+    'captchaSettings' => [
 
-        // The language reCAPTCHA will use. Default: 'en'.
-        // For supported languages, see: https://developers.google.com/recaptcha/docs/language
-        //'language' => 'en',
+        // ...
 
-        // The theme that reCAPTCHA will use. Default: 'light'.
-        //'theme' => 'light|dark',
+        'sproutformsgooglerecaptcha-googlerecaptcha' => [
 
-        // The size the reCAPTCHA will use. Default : normal
-        // This setting only applies to the v2_checkbox captcha
-        // See: https://developers.google.com/recaptcha/docs/display#render_param
-        //'size' => 'normal|compact',
+            // Enable the Captcha for use.
+            'enabled' => true,
 
-        // The position the reCAPTCHA will use. Default : bottomright
-        // This setting only applies to the v2_invisible captcha
-        //'badge' => 'bottomright|bottomleft|inline-badge|inline-text'
+            // The type of reCAPTCHA to use when validating a form
+            // Options: v2_checkbox, v2_invisible
+            'recaptchaType' => 'v2_checkbox',
 
-        // Enable this setting if you wish to manage CSS manually. Default: false
-        //'disableCss' => true|false,
+            // The siteKey from your reCAPTCHA settings: https://www.google.com/recaptcha/admin
+            'siteKey' => '',
+
+            // The secretKey from your reCAPTCHA settings: https://www.google.com/recaptcha/admin
+            'secretKey' => '',
+
+            // The language reCAPTCHA will use;use;use;use;. Default: 'en'.
+            // For supported languages, see: https://developers.google.com/recaptcha/docs/language
+            'language' => 'en',
+
+            // The theme that reCAPTCHA will use
+            // Options: light, dark
+            'theme' => 'light',
+
+            // The size the reCAPTCHA will use
+            // Options: normal, compact
+            // This setting only applies to the v2_checkbox captcha
+            // See: https://developers.google.com/recaptcha/docs/display#render_param
+            'size' => 'normal',
+
+            // The position the reCAPTCHA will use
+            // Options: bottomright, bottomleft, inline-badge, inline-text
+            // This setting only applies to the v2_invisible captcha
+            'badge' => 'bottomright',
+
+            // Enable this setting if you wish to manage CSS manually
+            'disableCss' => false
+        ]
+
+        // ...
     ]
+
+    // ...
 ];
